4154da402dc2d8f62ebf588378b41a7bce52da8a
Gayal Balasooriya <baigayal01@gmail.com>
2021-11-02 10:05:57 +0530

    component creation

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>hello world</h1>
+        <button>Button</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
