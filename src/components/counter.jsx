866f80b096892b79523be6b852a6cb2164c4ba98
Gayal Balasooriya <baigayal01@gmail.com>
2021-11-02 16:36:13 +0530

    Done until evet handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 
-class Counter extends React.Component {
+class Counter extends Component {
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  buttonClick = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
   render() {
     return (
       <React.Fragment>
-        <h1>hello world</h1>
-        <button>Button</button>
+        <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button onClick={this.buttonClick}>Button</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "m-2 badge bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
